a1c96a5e0eb84964b88d23fc15d71e2d0616aca0
Asir Faysal Rafin <00asir@gmail.com>
2021-08-07 00:24:15 +0800

    Added Recipe

---
+++ src/Meal.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react"
+
+export default function Meal({ meal }) {
+    const [imageUrl, setImageUrl] = useState("")
+
+    useEffect(() => {
+        fetch(
+            `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=c3fcfcb04efd479b93e8bdd5e680b8de&includeNutrition=false`
+        )
+        .then(response => response.json())
+        .then(data => {
+            setImageUrl(data.image)
+        })
+        .catch(() => {
+            console.log("error")
+        })
+    }, [meal.id])
+
+
+
+    return (
+        <article>
+            <h1>{meal.title}</h1>
+            <img src={imageUrl} alt="recipe" />
+            <ul className="instructions">
+                <li>Preparation time: {meal.readyInMinutes} mintues</li>
+                <li>Number of servings: {meal.servings}</li>
+            </ul>
+
+            <a href={meal.sourceUrl}>Go to Recipe</a>
+        </article>
+    )
+}
